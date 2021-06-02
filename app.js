f96365250d1b302f94fd023adacae17afa9ff48d
Shane Chou <shaioking@gmail.com>
2021-06-02 23:39:53 +0800

    Step 1

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+// middlewares
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  console.log("Hi, I am the second middleware.");
+  next;
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/test", {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000.");
+});
